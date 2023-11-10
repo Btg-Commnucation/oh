9ca191aefbf32407ad0d740245ccf1fa305e6fde
Tuvedunyel <mightygodx@gmail.com>
2023-11-10 16:41:13 +0100

    page bottom

--- front-page.php
+++ front-page.php
@@ -2,62 +2,62 @@
 <html <?php language_attributes() ?>>
 
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php the_title(); ?></title>
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<main id="front-page">
-  <img src="<?= get_template_directory_uri(); ?>/public/oh.svg" alt="Logo de Oh en blanc">
+  <?php wp_body_open(); ?>
+  <main id="front-page">
+    <img src="<?= get_template_directory_uri(); ?>/public/oh.svg" alt="Logo de Oh en blanc">
 
-	<?php if ( have_rows( 'menu_principal', 'option' ) ) : ?>
+    <?php if (have_rows('menu_principal', 'option')) : ?>
       <nav id="front-menu">
         <ul>
-			<?php while ( have_rows( 'menu_principal', 'option' ) ) :
-				the_row();
-				$link = get_sub_field( 'lien' );
-				if ( $link['title'] !== "Contact" ) : ?>
-                  <li>
-                    <a href="<?= esc_url( $link['url'] ); ?>" target="<?= esc_attr( $link['target'] ); ?>">
-						<?= esc_html( $link['title'] ); ?>
-                    </a>
-                  </li>
-                  <li class="separator"></li>
-				<?php endif ?>
-			<?php endwhile; ?>
+          <?php while (have_rows('menu_principal', 'option')) :
+            the_row();
+            $link = get_sub_field('lien');
+            if ($link['title'] !== "Contact") : ?>
+              <li>
+                <a href="<?= esc_url($link['url']); ?>" target="<?= esc_attr($link['target']); ?>">
+                  <?= esc_html($link['title']); ?>
+                </a>
+              </li>
+              <li class="separator"></li>
+            <?php endif ?>
+          <?php endwhile; ?>
         </ul>
       </nav>
-	<?php endif; ?>
-</main>
-<?php wp_footer(); ?>
-<footer class="footer-front">
-	<?php
-		$link_contact = get_field( 'nous_contacter', 'option' ); ?>
-  <section class="white-footer">
-    <p>Réalisation : <a href="https://btg-communication.fr" rel="noopener noreferrer" target="_blank">
-        btg communication
-      </a></p>
-    <ul class="links">
-      <li>
-		  <?php
-			  $link_mention = get_field( 'mention_legales', 'option' ); ?>
-        <a href="<?= esc_url( $link_mention['url'] ); ?>" target="<?= esc_attr( $link_mention['target'] ) ?>">
-			<?= esc_html( $link_mention['title'] ); ?>
-        </a>
-      </li>
-      <li class="separator"></li>
-      <li>
-        <a href="<?= esc_url( $link_contact['url'] ); ?>" target="<?= esc_attr( $link_contact['target'] ) ?>">
-			<?= esc_html( $link_contact['title'] ); ?>
-        </a>
-      </li>
-    </ul>
-  </section>
-</footer>
+    <?php endif; ?>
+  </main>
+  <?php wp_footer(); ?>
+  <footer class="footer-front">
+    <?php
+    $link_contact = get_field('nous_contacter', 'option'); ?>
+    <section class="white-footer">
+      <p>Réalisation : <a href="https://btg-communication.fr" rel="noopener noreferrer" target="_blank">
+          btg communication
+        </a></p>
+      <ul class="links">
+        <li>
+          <?php
+          $link_mention = get_field('mention_legales', 'option'); ?>
+          <a href="<?= esc_url($link_mention['url']); ?>" target="<?= esc_attr($link_mention['target']) ?>">
+            <?= esc_html($link_mention['title']); ?>
+          </a>
+        </li>
+        <li class="separator"></li>
+        <li>
+          <a href="<?= esc_url($link_contact['url']); ?>" target="<?= esc_attr($link_contact['target']) ?>">
+            <?= esc_html($link_contact['title']); ?>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </footer>
 
 </body>
 
--- page.php
+++ page.php
@@ -2,14 +2,14 @@
 
 <main id="page-template">
   <section class="top">
-	  <?php $image_top = get_field( 'image_haut_de_page' ); ?>
+    <?php $image_top = get_field('image_haut_de_page'); ?>
     <div class="top-image">
-      <img src="<?= esc_url( $image_top['url'] ); ?>" alt="<?= esc_attr( $image_top['alt'] ); ?>" class="image">
+      <img src="<?= esc_url($image_top['url']); ?>" alt="<?= esc_attr($image_top['alt']); ?>" class="image">
       <img src="<?= get_template_directory_uri() ?>/public/oh.svg" alt="Logo Oh en blanc" class="logo-page">
     </div>
     <div class="top-content">
-        <span class="point"></span>
-        <div class="border-wrapper"></div>
+      <span class="point"></span>
+      <div class="border-wrapper"></div>
       <?php the_field('texte_haut_de_page'); ?>
       <span class="point"></span>
     </div>
@@ -33,6 +33,51 @@
       </div>
     </div>
   </article>
+  <section class="second-point">
+    <div class="left-second-point">
+      <h2>
+        <span>«  <br /></span>
+        <?php the_field('titre_second_point'); ?>
+      </h2>
+      <div class="subTitle">
+        <strong class="step">02 <span></span></strong>
+        <?php the_field('sous_titre_second_point'); ?>
+      </div>
+      <div class="second-point-content">
+        <?php the_field('texte_second_point'); ?>
+      </div>
+    </div>
+    <div class="right-second-point">
+      <?php $image_second = get_field('image_second_point'); ?>
+      <img src="<?= esc_url($image_second['url']); ?>" alt="<?= esc_attr($image_second['alt']); ?>">
+    </div>
+  </section>
+  <section class="third-point">
+    <div class="container logo-line">
+      <img src="<?= get_template_directory_uri() ?>/public/oh-blue.svg" alt="Logo Oh en bleu">
+      <div class="line"></div>
+    </div>
+    <?php if (have_rows('image_troisieme_point')) : ?>
+      <div class="galleries-page">
+        <ul class="gallerie-img__container">
+          <?php while (have_rows('image_troisieme_point')) : the_row();
+            $image = get_sub_field('image'); ?>
+            <li>
+              <img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>">
+            </li>
+          <?php endwhile; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+    <div class="subTitle">
+      <strong class="step">03 <span></span></strong>
+      <h3><?php the_field('sous_titre_troisieme_point'); ?></h3>
+    </div>
+    <div class="third-point-content">
+      <div><?php the_field('premier_texte_troisieme_point'); ?></div>
+      <div><?php the_field('premier_texte_troisieme_point'); ?></div>
+    </div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
