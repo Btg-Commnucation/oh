3f6879dbefbd8cfd937c949b1621c3840659a3a4
Tuvedunyel <mightygodx@gmail.com>
2023-11-14 16:54:19 +0100

    photographie 2 point responsive

--- template-photographie.php
+++ template-photographie.php
@@ -17,7 +17,7 @@ Template Name: Photographie
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
-    <main id="photographie">
+    <div id="photographie">
         <section class="splide">
             <div class="splide__track">
                 <ul class="splide__list">
@@ -44,9 +44,31 @@ Template Name: Photographie
                         <?php $img_deux_gal_deux = get_field('image_2_gallerie_2'); ?>
                         <img src="<?= esc_url($img_deux_gal_deux['url']); ?>" alt="<?= esc_attr($img_deux_gal_deux['alt']); ?>">
                     </li>
+                    <li class="section gallerie_trois splide__slide">
+                        <?php $img_gal_3 = get_field('image_1_gallerie_3');
+                        $img_2_gal_3 = get_field('image_2_gallerie_3');
+                        ?>
+                        <div class="img-container">
+                            <img src="<?= esc_url($img_gal_3['url']); ?>" alt="<?= esc_attr($img_gal_3['alt']); ?>">
+                            <img src="<?= esc_url($img_2_gal_3['url']); ?>" alt="<?= esc_attr($img_2_gal_3['alt']); ?>">
+                        </div>
+                        <div class="logo-line">
+                            <img src="<?= get_template_directory_uri(); ?>/public/oh-blue.svg" alt="Logo Oh en bleu">
+                            <span class="line"></span>
+                        </div>
+                    </li>
+                    <li class="section gallerie_quatre splide__slide">
+                        <h2><?php the_field('titre_bas_de_page'); ?></h2>
+                        <?php if (have_rows('images_bas_de_page')) :
+                            while (have_rows('images_bas_de_page')) : the_row(); ?>
+                                <?php $img_bas_de_page = get_sub_field('image'); ?>
+                                <img src="<?= esc_url($img_bas_de_page['url']); ?>" alt="<?= esc_attr($img_bas_de_page['alt']); ?>">
+                        <?php endwhile;
+                        endif; ?>
+                    </li>
                 </ul>
             </div>
         </section>
-    </main>
+    </div>
 
     <?php get_footer(); ?>
